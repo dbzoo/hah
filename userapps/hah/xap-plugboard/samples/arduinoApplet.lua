02d60a9d0674b56ef17a91c2185d7a3a2aaaad6a
dbzoo.com <dbzoo.com@64644fc6-01da-11df-bca2-d32778fd5087>
2011-10-10 17:23:51 +0000

    Added arduinoApplet.lua sample
    git-svn-id: https://livebox-hah.googlecode.com/svn/trunk@282 64644fc6-01da-11df-bca2-d32778fd5087

---
+++ userapps/hah/xap-plugboard/samples/arduinoApplet.lua
@@ -0,0 +1,53 @@
+--[[ 
+    Arduino to xAP Endpoint mapping
+    Use in conjunction with the singleLedControl.pde sketch
+--]]
+
+module(...,package.seeall)
+
+require("xap")
+require("xap.bsc")
+
+info={
+   version="1.0", description="Arduino control"
+}
+port="/dev/ttyUSB0"
+
+function cmd(endpoint)
+   xap.sendShort(string.format([[
+xap-header
+{
+class=Serial.Comms
+target=dbzoo.livebox.serial
+}
+Serial.Send
+{
+port=%s
+data=led %s\n
+}]], port, endpoint.state))
+end
+
+function init()
+   local serialSetup = [[
+xap-header
+{
+class=Serial.Comms
+target=dbzoo.livebox.serial
+}
+Serial.Setup
+{
+      port=%s
+      baud=9600
+      stop=1
+      databits=8
+      parity=none
+      flow=none
+}]]
+   xap.sendShort(string.format(serialSetup, port))
+
+   bsc.Endpoint{ source="dbzoo.livebox.arduino:led", 
+		 direction=bsc.OUTPUT, 
+		 type=bsc.BINARY, 
+		 cmdCB=cmd
+	      }
+end

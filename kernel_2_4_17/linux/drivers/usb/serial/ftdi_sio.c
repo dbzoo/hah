ad7ab8e49eef17d87b8fb15b4a01874515bbc65f
dbzoo.com <dbzoo.com@64644fc6-01da-11df-bca2-d32778fd5087>
2012-03-03 20:01:38 +0000

    Hacking in Arduino UNO support to the kernel driver
    git-svn-id: https://livebox-hah.googlecode.com/svn/trunk@387 64644fc6-01da-11df-bca2-d32778fd5087

--- kernel_2_4_17/linux/drivers/usb/serial/ftdi_sio.c
+++ kernel_2_4_17/linux/drivers/usb/serial/ftdi_sio.c
@@ -138,6 +138,7 @@ static __devinitdata struct usb_device_id id_table_sio [] = {
    
 static __devinitdata struct usb_device_id id_table_8U232AM [] = {
 	{ USB_DEVICE(FTDI_VID, FTDI_8U232AM_PID) },
+	{ USB_DEVICE(0x2341, 0x1) },  // Arduino UNO
 	{ }						/* Terminating entry */
 };
 
@@ -145,6 +146,7 @@ static __devinitdata struct usb_device_id id_table_8U232AM [] = {
 static __devinitdata struct usb_device_id id_table_combined [] = {
 	{ USB_DEVICE(FTDI_VID, FTDI_SIO_PID) },
 	{ USB_DEVICE(FTDI_VID, FTDI_8U232AM_PID) },
+	{ USB_DEVICE(0x2341, 0x1) }, // Arduino UNO
 	{ }						/* Terminating entry */
 };
 

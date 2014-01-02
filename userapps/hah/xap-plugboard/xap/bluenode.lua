862dd79e5b8382792acae847cbfaa7a728760ba2
dbzoo.com <dbzoo.com@64644fc6-01da-11df-bca2-d32778fd5087>
2014-01-02 10:24:52 +0000

    Custom reaper for the bluenode endpoints
    git-svn-id: https://livebox-hah.googlecode.com/svn/trunk@528 64644fc6-01da-11df-bca2-d32778fd5087

--- userapps/hah/xap-plugboard/xap/bluenode.lua
+++ userapps/hah/xap-plugboard/xap/bluenode.lua
@@ -1,3 +1,5 @@
+local DEBUG = rawget(_G,'_DEBUG')
+
 module("bluenode", package.seeall)
 
 require("xap.bsc")
@@ -23,13 +25,17 @@ local nodes = {
 function BlueNode:build(...)
    Nodule.build(self, ...)
 
-   -- Force reapable.
-   -- Seconds, must be > sketch polling time.
-   self.cfg.ttl = 35
-
+   ttl = self.cfg.ttl or 35
+   
    for device in pairs(self.cfg.endpoints) do
-      self:add {key=device, direction=bsc.INPUT, type=bsc.BINARY, displaytext=device}
+      self:add {key=device, direction=bsc.INPUT, type=bsc.BINARY, displaytext=device, lastSeen=os.time(), isAlive=false, ttl=ttl}
    end
+
+   --  use default reaper as it would expire ALL devices at the same time.
+   self.cfg.ttl = nil
+
+   -- Check for non-responding Endpoints every 10 seconds
+   xap.Timer(BlueNode.reaper, 10, self):start()
 end
 
 function BlueNode:process(data)
@@ -42,20 +48,34 @@ function BlueNode:process(data)
     } payload;
 --]]
     local device = string.format("%x:%x:%x", jeenode.bitslicer(data,16,8,24))
-    -- Update to say this device is now present.
+
+    -- Update to say this device is now present, its alive !
+    self[device].lastSeen = os.time()
+    self[device].isAlive = true
     Nodule.process(self,{[device]=bsc.STATE_ON})
 end
 
--- Override default expiry mechanism.
--- We don't want to go unknown, on/off please.
-function BlueNode:expire()
-   if DEBUG then
-      print('expire '.. tostring(self))
-   end
-   self.isalive = false
-   for name in pairs(self.cfg.endpoints) do
-      local e = self[name]
-      e.state = bsc.STATE_OFF
-      e:sendEvent()
-   end
+-- Custom reaper that can deal with multiple endpoints on the same base target
+-- Don't fear the reaper - Bluetooth oyster cult. :)
+
+function BlueNode:reaper(self)
+    local now = os.time()
+    for key in pairs(self.cfg.endpoints) do
+         local device = self[key]
+	 if DEBUG then
+	    print(string.format("BlueNode reaper %s alive=%s ttl=%s timeout=%s", 
+				key,
+				utils.choose(device.isAlive == true,"true","false"),
+                                device.ttl,
+				now-device.lastSeen))
+	    
+	 end
+	if device.isAlive and now - device.lastSeen > device.ttl then
+	   if DEBUG then
+	      print('expire '.. key)
+	   end
+	   device:setState(bsc.STATE_OFF)
+           device:sendEvent()
+        end
+    end
 end

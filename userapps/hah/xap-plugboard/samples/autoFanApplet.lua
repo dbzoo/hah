42da581c467dd69a640cfa78ede0f3c12d6061b9
dbzoo.com <dbzoo.com@64644fc6-01da-11df-bca2-d32778fd5087>
2014-01-16 05:20:22 +0000

    Adding fan controller applet
    git-svn-id: https://livebox-hah.googlecode.com/svn/trunk@539 64644fc6-01da-11df-bca2-d32778fd5087

---
+++ userapps/hah/xap-plugboard/samples/autoFanApplet.lua
@@ -0,0 +1,47 @@
+--[[
+  Control a fan based on a temperature sensor
+  
+  When the temperature gets about 33c turn on relay4 which controls an exhaust fan
+  run the fan until the temperature drop to 26c or the fan has run for 5 minutes.
+--]]
+module(...,package.seeall)
+require("xap")
+info = {version="0.02", description="Auto Fan"}
+
+local sensor = "dbzoo.livebox.Controller:1wire.1"
+local fan = "dbzoo.livebox.Controller:relay.4"
+local autoShut = nil
+local prevTemp = 0
+
+function fanOff()
+  bsc.sendState(fan, "off")
+  if autoShut ~= nil then
+    autoShut:delete()
+    autoShut = nil
+  end
+end
+
+function tempEvent(frame)
+  temp = tonumber(frame:getValue("input.state","text"))
+  --print(string.format("Temp %s", temp))
+  if temp < prevTemp then
+     if temp < 26 then
+        fanOff()
+     end
+  elseif temp > prevTemp then
+    if temp > 33 and autoShut == nil then
+        bsc.sendState(fan, "on")
+        -- Don't run the system longer than 5 minutes
+        -- Could be we just can't cool down to the lower limit
+        autoShut = xap.Timer(fanOff, 60*5):start()
+    end
+  end
+  prevTemp = temp
+end
+
+function init()
+  f = xap.Filter()
+  f:add("xap-header","source", sensor)
+  f:add("xap-header","class","xAPBSC.event")
+  f:callback(tempEvent)
+end
